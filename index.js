1db9b0ca56b85eebcb5ef5e97db5226f1ec1c254
rimasultana <rimasultana20225@gmail.com>
2024-03-01 22:09:49 +0600

    numbers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// let x = 3.45;
-// let y = 3;
+// // let x = 3.45;
+// // let y = 3;
+// // document.getElementById("demo").innerHTML = x + " <br> " + y;
+// // let x = 123e5;
+// // let y = 123e-5;
 // document.getElementById("demo").innerHTML = x + " <br> " + y;
-let x = 123e5;
-let y = 123e-5;
-document.getElementById("demo").innerHTML = x + " <br> " + y;
+let x = 0.4 + .45;
+document.getElementById("demo").innerHTML =0.4 + .45 + x;
