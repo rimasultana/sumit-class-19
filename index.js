c4a78f4181ffa7f7a3f720adf5b556e18a7c50f2
rimasultana <rimasultana20225@gmail.com>
2024-03-01 22:06:46 +0600

    decimal

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-let x = 3.45;
-let y = 3;
+// let x = 3.45;
+// let y = 3;
+// document.getElementById("demo").innerHTML = x + " <br> " + y;
+let x = 123e5;
+let y = 123e-5;
 document.getElementById("demo").innerHTML = x + " <br> " + y;
